932b00d6a14a0ecbd096b9e092a5296ca45806b3
icarofr <icaro@gmx.fr>
2022-08-24 10:49:49 +0200

    fixed broken seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,13 @@ puts "delete db"
 Basket.destroy_all
 User.destroy_all
 
-user = User.create!(first_name: "charles", last_name: "Martel", email: "charles@gmail.com", password: '123456')
+first_name = ["Myah","Timothy","Jonas","Maura","Silas","Rudy","Emalee","Ashli","Andy","Greta"]
+last_name = ["Harris","Hernandez","Hill","Hines","Hinton","Hobbs","Hodge","Hoffman","Hogan","Holcomb"]
+
+(0..9).each do |i|
+    puts "create user #{i+1}"
+    User.create!(email: "email#{i}@myjordans.com", password: "password#{i}", first_name: first_name[i], last_name: last_name[i])
+end
 
 require "json"
 require "open-uri"
@@ -21,20 +27,21 @@ count = 0
 
 loop do
     sneakers.each do |sneaker|
-        if count > 50 then
+        if count >= 50 then
             exit
         end
         if sneaker['main_picture']['url'].include?("data:image") then
             next
         end
         count += 1
+        puts "create basket #{count}"
         Basket.create!({
             name: sneaker['model'],
             description: sneaker['description'],
             size: rand(38..45),
             price_per_day: sneaker['price'] / 10,
             image: "https://sold-out.io#{sneaker['main_picture']['url']}",
-            user_id: user.id
+            user_id: User.all.sample.id
         })
     end
 end
