670e2955158e22784fde9fbcd3fde19204021bc1
icarofr <icaro@gmx.fr>
2022-08-26 15:50:37 +0200

    fixed navbar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,51 +1,43 @@
 class BookingsController < ApplicationController
+  def new
+    @basket = Basket.find(params[:basket_id])
+    @booking = Booking.new
+  end
 
-	def new
-		@basket = Basket.find(params[:basket_id])
-		@booking = Booking.new
-	end
+  def create
+    @basket = Basket.find(params[:basket_id])
+    @booking = Booking.new(bookings_params)
+    @booking.basket = @basket
+    @booking.user = current_user
+    @booking.status = "attente"
+    if @booking.save
+      redirect_to dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-	def create
-		@basket = Basket.find(params[:basket_id])
-		@booking = Booking.new(bookings_params)
-		@booking.basket = @basket
-		@booking.user = current_user
-		@booking.status = "pending"
-		if @booking.save
-			redirect_to dashboard_path
-		else
-			render :new, status: :unprocessable_entity
-		end
-	end
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "accepted")
+    redirect_to dashboard_path if @booking.save
+  end
 
-	def accept
-		@booking = Booking.find(params[:id])
-		@booking.update(status: "accepted")
-		if @booking.save 
-			redirect_to dashboard_path
-		end
-	end
+  def decline
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "declined")
+    redirect_to dashboard_path if @booking.save
+  end
 
-	def decline
-		@booking = Booking.find(params[:id])
-		@booking.update(status: "declined")
-		if @booking.save 
-			redirect_to dashboard_path
-		end
-	end
+  def cancel
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "canceled")
+    redirect_to dashboard_path if @booking.save
+  end
 
-	def cancel
-		@booking = Booking.find(params[:id])
-		@booking.update(status: "canceled")
-		if @booking.save 
-			redirect_to dashboard_path
-		end
-	end
+  private
 
-
-	private
-
-	def bookings_params
-		params.require(:booking).permit(:start_date, :end_date)
-	end
+  def bookings_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,8 @@
 class Booking < ApplicationRecord
-  STATUS = %w(attente acceptée declinée annulée)
+  STATUS = %w[attente acceptée declinée annulée]
 
   belongs_to :user
   belongs_to :basket
 
-  validates :status, inclusion: { in: Booking::STATUS } 
+  validates :status, inclusion: { in: Booking::STATUS }
 end
